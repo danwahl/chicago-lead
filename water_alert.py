5a28cff1bed0b75a90c162ca219a317dbe6baf09
Dan Wahl <drwahl@gmail.com>
2017-07-22 11:32:51 -0500

    updates for automation

--- water_alert.py
+++ water_alert.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     driver.get(site)
     assert title in driver.title
     
-    for r in itertools.product(range(17, 18), range(1000)):
+    for r in itertools.product(range(15, 17), range(500)):
         # generate project id
         pid = '{:02d}01{:03d}'.format(r[0], r[1])
         
